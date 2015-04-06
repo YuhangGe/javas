2b663cf49e241b7f81a16f5aaaa18c9981f0f4af
YuhangGe <abeyuhang@gmail.com>
2015-04-07 00:35:56 +0800

    init

--- src/base/manager.js
+++ src/base/manager.js
@@ -1,10 +1,8 @@
 var _ = require('../util/util.js');
+var Config = require('../util/conf.js');
 
 module.exports = Manager;
 
-var LOOP_INTERVAL_TIME = 10;
-var LOOP_MAX_FPS = Math.floor(1000 / LOOP_INTERVAL_TIME);
-var LOOP_HALF_INTERVAL_TIME = Math.floor(LOOP_INTERVAL_TIME);
 
 var defaultOptions = {
   FPS: 30,
@@ -13,11 +11,12 @@ var defaultOptions = {
 };
 
 function Manager(target, options) {
-  this.options = _.mergeOptions(options, Manager.defaultOptions);
+  this.options = _.mergeOptions(options, defaultOptions);
 
   var FPS = this.options.FPS;
+  var LOOP_MAX_FPS = Math.floor(1000 / Config.loopIntervalTime);
   if (FPS > LOOP_MAX_FPS) {
-    console.warn('已忽略的option, FPS 当前版本最大只能设置为', LOOP_MAX_FPS);
+    console.warn('已忽略的option, FPS 最大只能设置为', LOOP_MAX_FPS);
     FPS = LOOP_MAX_FPS;
   } else if (FPS <= 0) {
     _.error('错误的option, FPS 必须大于等于1')
@@ -31,13 +30,13 @@ function Manager(target, options) {
   this.container = target.parentElement;
 
   this.loopDelegate = _.bind(this, this._loopHandler);
-  //this.loopTimeout = null;
   this.loopIntervalTime = Math.floor(1000 / FPS);
-  this.loopRunning = false;
-  this.loopStartTime = 0;
-  this.loopPreviousTime = 0;
-  this.loopTotalDurationTime = 0;
   this.loopNextIntervalTime = 0;
+  this.loopStartTime = 0;
+  this.loopConfigTime = Config.loopIntervalTime;
+  this.loopHalfConfigTime = Math.floor(Config.loopIntervalTime / 2);
+
+  this.loopRunning = false;
 
   this.shapeList = [];
 
@@ -94,38 +93,35 @@ var __manager_prototype = Manager.prototype = {
   },
   _loopHandler: function () {
     var curTime = Date.now();
-
-    var deltaTime = this.loopNextIntervalTime - this.loopPreviousTime;
-
-    if (deltaTime < LOOP_HALF_INTERVAL_TIME) {
-
-      //todo
-
-      this.loopNextIntervalTime += this.loopIntervalTime;
+    var deltaTime = this.loopNextIntervalTime - curTime;
+    if (deltaTime < this.loopHalfConfigTime) {
+      if (this.loopRunning) {
+        this._loopRender(curTime);
+      }
+      this.loopNextIntervalTime = curTime + this.loopIntervalTime;
     }
-
-    this.loopTotalDurationTime = curTime - this.loopStartTime;
+  },
+  _loopRender: function (curTime) {
 
   },
   _loopCheck: function () {
+    //todo
 
+    //this.loopRunning = true/false;
   },
   _loopStart: function () {
     this.loopStartTime = Date.now();
-    this.loopPreviousTime = this.loopStartTime;
     this.loopNextIntervalTime = this.loopStartTime + this.loopIntervalTime;
     /*
      * 开始主线程循环。
      */
-    setInterval(this.loopDelegate, LOOP_INTERVAL_TIME);
+    setInterval(this.loopDelegate, this.loopConfigTime);
   },
   loop: function () {
     if (this.loopRunning) {
       return;
     }
-    if (this._loopCheck()) {
-      this._loopStart();
-    }
+    this._loopCheck();
   }
 };
 
--- src/util/conf.js
+++ src/util/conf.js
@@ -1,5 +1,14 @@
 var Config = {
-  debug: true
+  debug: true,
+  /*
+   * 这里设置Manager的循环时间。10ms代表每秒loop的次数大概是100次，
+   * 也就是，Manager可以达到的最大FPS是100。
+   * 10ms虽然很频繁，但只进行了两行if一行加法的计算（见manager.js中_loopHandler函数代码），通常情况下，你不需要修改这个参数。
+   * 但考虑到处女座同学，可能就希望把这个值改大些，以避免频繁运算；
+   *   或者某些情况下，需要超过100FPS的极限；
+   *   这里仍然提供了可以配置的选项。
+   */
+  loopIntervalTime: 10
 };
 
 module.exports = Config;
